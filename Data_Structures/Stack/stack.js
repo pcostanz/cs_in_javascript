1956e15f5403ceeb9a05b94e0590d94c23d4a594
pcostanz <costanzo.patrick@gmail.com>
2013-12-28 13:00:33 -0800

    added print function to stack

--- Data_Structures/Stack/stack.js
+++ Data_Structures/Stack/stack.js
@@ -1,30 +1,45 @@
 // Stack constructor function
 
 var Stack = function() {
-	this.tail = null;
+	this.top = null;
 	this.size = 0;
 }
 
 Stack.prototype.push = function(value) {
-	if (this.tail === null) {
-		this.tail = {prev: null, data: value}
+	if (this.top === null) {
+		this.top = {prev: null, data: value}
 	} else {
-		this.tail = {prev: this.tail, data: value}
+		this.top = {prev: this.top, data: value}
 	}
 
 	this.size += 1;
 }
 
 Stack.prototype.pop = function() {
-	if (this.tail === null) {
-		console.log("Error, stack is empty");
+	if (this.top === null) {
+		console.log("Error, underflow");
 		return;
 	} else {
-		var popped = this.tail.data;
-		this.tail = this.tail.prev;
+		var popped = this.top.data;
+		this.top = this.top.prev;
+		this.size -= 1;
 		console.log(popped);
 		return popped;
 	}
+}
+
+Stack.prototype.print = function() {
+
+	console.log("printing");
+	var current = this.top;
+	var tempStack = [];
+
+	while (current !== null) {
+		tempStack.unshift(current.data);
+		current = current.prev;
+	}
+	console.log(tempStack);
+	return tempStack;
 }
 
 var stack = new Stack();
